0b5552593d81893fb5b83f0e578636df2c140bd9
arne <arne@mintlab.nl>
2017-11-08 17:26:35 +0100

    Clean up file

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { BrowserRouter, Match, Miss } from 'react-router';
 import './styles/css/style.css';
 import Diary from './components/Diary';
 import NotFound from './components/NotFound';
-import addPage from './components/addPage';
+import AddPage from './components/AddPage';
 
 const Root = () => {
     return (
         <BrowserRouter>
             <div>
                 <Match exactly pattern="/" component={Diary} />
-                <Match exactly pattern="/addPage" component={addPage} />
+                <Match exactly pattern="/addPage" component={AddPage} />
                 <Miss component={NotFound} />
             </div>
         </BrowserRouter>
