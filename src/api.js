50cf74c7b37ddf128d4eb61b00997e885914d79f
arnedeboer <arne@mintlab.nl>
2018-07-09 14:37:41 +0200

    Ensure empty text fields are submitted as null

--- src/api.js
+++ src/api.js
@@ -1,73 +1,64 @@
-export const fetchAndSetItems = (itemType, setItems, filters) => {
-  return fetch(`/api/${itemType}/filter`, {
+const nullifyStrings = data => {
+  Object.keys(data).forEach(thing => {
+    const value = data[thing];
+
+    data[thing] = value === '' ? null : value;
+  });
+
+  return data;
+};
+
+const performFetch = (itemType, path, data) =>
+  fetch(`/api/${itemType}/${path}`, {
     method: "POST",
-    body: JSON.stringify(filters),
+    body: JSON.stringify(nullifyStrings(data)),
     headers: {
       "Content-Type": "application/json"
     }
-  })
-  .then(res => {
-    if(res.status === 200) {
-      return res.json().then(results => {
-        setItems(itemType, results);
+  });
+
+export const fetchAndSetItems = (itemType, setItems, filters) =>
+  performFetch(itemType, 'filter', filters)
+    .then(res => {
+      if(res.status === 200) {
+        return res.json().then(results => {
+          setItems(itemType, results);
+          return 'success';
+        });
+      }
+
+      return 'failed';
+    });
+
+export const createItem = (itemType, clearForm, itemData) =>
+  performFetch(itemType, 'create', itemData)
+    .then(res => {
+      if (res.status === 201) {
+        clearForm(itemType);
         return 'success';
-      });
-    }
+      }
 
-    return 'failed';
-  });
-};
+      return 'failed';
+    });
 
-export const createItem = (itemType, clearForm, itemData) => {
-  return fetch(`/api/${itemType}/create`, {
-    method: "POST",
-    body: JSON.stringify(itemData),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  .then(res => {
-    if (res.status === 201) {
-      clearForm(itemType);
-      return 'success';
-    }
+export const editItem = (itemType, editItem, itemData) =>
+  performFetch(itemType, 'edit', itemData)
+    .then(res => {
+      if (res.status === 201) {
+        editItem(itemType, itemData);
+        return 'success';
+      }
 
-    return 'failed';
-  });
-};
+      return 'failed';
+    });
 
-export const editItem = (itemType, editItem, itemData) => {
-  return fetch(`/api/${itemType}/edit`, {
-    method: "POST",
-    body: JSON.stringify(itemData),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  .then(res => {
-    if (res.status === 201) {
-      editItem(itemType, itemData);
-      return 'success';
-    }
+export const setItemToFalse = (itemType, deleteItem, itemData) =>
+  performFetch(itemType, 'delete', itemData)
+    .then(res => {
+      if (res.status === 201) {
+        deleteItem(itemType, itemData.id);
+        return 'success';
+      }
 
-    return 'failed';
-  });
-};
-
-export const setItemToFalse = (itemType, deleteItem, itemData) => {
-  return fetch(`/api/${itemType}/delete`, {
-    method: "POST",
-    body: JSON.stringify(itemData),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  .then(res => {
-    if (res.status === 201) {
-      deleteItem(itemType, itemData.id);
-      return 'success';
-    }
-
-    return 'failed';
-  });
-};
+      return 'failed';
+    });
